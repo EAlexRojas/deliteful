c786f1f3e0f1af01408f89bafa4367b489015c3a
Edward Alexander Rojas <alexrojas235@gmail.com>
2015-03-13 14:41:11 +0100

    Testing only on iOS and IE9

--- tests/intern.js
+++ tests/intern.js
@@ -21,19 +21,19 @@ define({
 
 		// Desktop.
 		// Not running on IE9 since Widget-attr test depends on domClass methods only available in IE10_
-		{ browserName: "internet explorer", version: "11", platform: "Windows 8.1", requireWindowFocus: "true",
-			name : "deliteful"},
-		{ browserName: "internet explorer", version: "10", platform: "Windows 8", requireWindowFocus: "true",
-			name : "deliteful"},
+		//{ browserName: "internet explorer", version: "11", platform: "Windows 8.1", requireWindowFocus: "true",
+		//	name : "deliteful"},
+		//{ browserName: "internet explorer", version: "10", platform: "Windows 8", requireWindowFocus: "true",
+		//	name : "deliteful"},
 		{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
-		{ browserName: "firefox", version: "31", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ],
-			// default selenium-version is currently 2.40.0; some Combobox functional tests fail with
-			// 2.40.0, while they pass with 2.41.0.
-			"selenium-version": "2.41.0",
-			name : "deliteful"},
-		{ browserName: "chrome", version: "32", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ],
-			name : "deliteful"},
-		{ browserName: "safari", version: "7", platform: [ "OS X 10.9" ], name : "deliteful"}
+		//{ browserName: "firefox", version: "31", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ],
+		//	// default selenium-version is currently 2.40.0; some Combobox functional tests fail with
+		//	// 2.40.0, while they pass with 2.41.0.
+		//	"selenium-version": "2.41.0",
+		//	name : "deliteful"},
+		//{ browserName: "chrome", version: "32", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ],
+		//	name : "deliteful"},
+		//{ browserName: "safari", version: "7", platform: [ "OS X 10.9" ], name : "deliteful"}
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
