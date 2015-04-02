ea63efc62366a78fdcee1543e1980635b74330d6
Edward Alexander Rojas <alexrojas235@gmail.com>
2015-04-02 17:28:10 +0200

    Accordion first version. All collapsibles works. Single Open to complete

---
+++ Panel.js
@@ -0,0 +1,17 @@
+define(["dcl/dcl",
+	"delite/register",
+	"delite/DisplayContainer",
+	"delite/handlebars!./Panel/Panel.html",
+    "delite/theme!./Panel/themes/{{theme}}/Panel.css"
+], function (dcl, register, DisplayContainer, template) {
+
+	var Panel = dcl(DisplayContainer, {
+		baseClass: "d-panel",
+		label: "",
+		icon: "",
+		icon2: "", //When used inside an accordion
+		template: template
+	});
+
+	return register("d-panel", [HTMLElement, Panel]);
+});
