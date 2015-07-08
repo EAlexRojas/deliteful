4dd47e78d8e038cbb55210cdb130a83df9138f77
Edward Alexander Rojas <alexrojas235@gmail.com>
2015-07-08 11:42:01 +0200

    Testing only ViewStack and SwapView

--- tests/functional/all.js
+++ tests/functional/all.js
@@ -1,16 +1,16 @@
 // Listing of all the deliteful functional tests
 define([
-	"./StarRating",
-	"./ScrollableContainer",
-	"./list/all",
-	"./Toaster",
-	"./Select",
-	"./Slider",
-	"./SidePane",
-	"./Checkbox",
-	"./Switch",
-	"./Button",
-	"./Combobox",
+	//"./StarRating",
+	//"./ScrollableContainer",
+	//"./list/all",
+	//"./Toaster",
+	//"./Select",
+	//"./Slider",
+	//"./SidePane",
+	//"./Checkbox",
+	//"./Switch",
+	//"./Button",
+	//"./Combobox",
 	"./SwapView",
-	"./features"
+	//"./features"
 ]);
