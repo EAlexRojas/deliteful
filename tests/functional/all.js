3a9f2e5a11288be1570cf63deb1aac9d8c62333b
Edward Alexander Rojas <alexrojas235@gmail.com>
2015-07-17 17:12:45 +0200

    Testing only Accordion

--- tests/functional/all.js
+++ tests/functional/all.js
@@ -1,17 +1,17 @@
 // Listing of all the deliteful functional tests
 define([
 	"./Accordion",
-	"./StarRating",
-	"./ScrollableContainer",
-	"./list/all",
-	"./Toaster",
-	"./Select",
-	"./Slider",
-	"./SidePane",
-	"./Checkbox",
-	"./Switch",
-	"./Button",
-	"./Combobox",
-	"./SwapView",
-	"./features"
+	//"./StarRating",
+	//"./ScrollableContainer",
+	//"./list/all",
+	//"./Toaster",
+	//"./Select",
+	//"./Slider",
+	//"./SidePane",
+	//"./Checkbox",
+	//"./Switch",
+	//"./Button",
+	//"./Combobox",
+	//"./SwapView",
+	//"./features"
 ]);
